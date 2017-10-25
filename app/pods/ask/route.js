f37454359e852d3b88054c4668ad3c05a1ca235a
Sergey Sherby <sergey.sherby@sportsfusion.eu>
2017-10-26 01:05:23 +0300

    First commit

---
+++ app/pods/ask/route.js
@@ -0,0 +1,55 @@
+import Ember from 'ember';
+
+const {
+  inject: { service },
+  isPresent
+} = Ember;
+
+export default Ember.Route.extend({
+  websockets: service(),
+  state: false,
+  questions: null,
+
+  beforeModel(){
+    const socket = this.get('websockets').socketFor('ws://localhost:7000/');
+    const asocket = this.get('websockets').socketFor('ws://localhost:1111/');
+
+    socket.on('message', function (message) {
+      message = JSON.parse(message.data);
+      console.log(message);
+      if(message.type === "interview") {
+        this.set('currentModel.state', true);
+        this.set('currentModel.questions', message.questions);
+        this.set("ans",null);
+      } else if(message.type === "stop") {
+        this.set('currentModel.state',false);
+        this.set('currentModel.questions', null)
+      }}, this);
+
+    this.set('socketRef', asocket);
+  },
+
+  model(){
+    return {
+      state: false,
+      questions: null
+    }
+  },
+
+  actions: {
+    vote(val){
+      this.set("ans",val);
+    },
+
+    answer(){
+      const ans = this.get('ans');
+      if(isPresent(ans)){
+          this.get('socketRef').send(JSON.stringify({
+          type: "answer",
+          id: ans
+        }))
+      }
+      this.set('currentModel.state',false);
+    }
+  }
+});
